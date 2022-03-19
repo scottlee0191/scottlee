2b41918a3b738b533d2dd5c1c6301c612673b0f4
scottlee0191 <scottlee0191@gmail.com>
2022-03-19 08:13:00 +0700

    add project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,9 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'A Personal Blog',
+    description: `A blog that record my knowledge share my experience during working. Blog built with tailwindcss, nextjs, reactjs, markdown.`,
+    imgSrc: '/static/images/blog.png',
+    href: 'https://scottlee0191.github.io/scottlee/',
   },
 ]
 
