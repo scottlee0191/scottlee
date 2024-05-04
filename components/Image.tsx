0d5929b2dcccb1f43c415b8bbf8fa168b18c60f1
scottlee <scottlee0191@gmail.com>
2024-05-04 09:33:24 +0700

    fix image path

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,8 +1,12 @@
 import NextImage, { ImageProps } from 'next/image'
 
 const Image = ({ src, ...rest }: ImageProps) => {
+  console.log('🚀 ~ Image ~ rest:', rest)
+  console.log('🚀 ~ Image ~ src:', `${process.env.NEXT_PUBLIC_PAGE_HOST}/scottlee${src}`)
   const imageSrc =
-    process.env.NODE_ENV === 'production' ? `${process.env.NEXT_PUBLIC_PAGE_HOST}/${src}` : src
+    process.env.NODE_ENV === 'production'
+      ? `${process.env.NEXT_PUBLIC_PAGE_HOST}/scottlee${src}`
+      : src
   return <NextImage src={imageSrc} {...rest} />
 }
 
