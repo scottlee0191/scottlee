df9516c283e8b38c99b5dc5a649b0fd1d220eb48
scottlee <scottlee0191@gmail.com>
2024-05-04 08:48:54 +0700

    fix public path

--- components/Image.tsx
+++ components/Image.tsx
@@ -2,9 +2,7 @@ import NextImage, { ImageProps } from 'next/image'
 
 const Image = ({ src, ...rest }: ImageProps) => {
   const imageSrc =
-    process.env.NODE_ENV === 'production'
-      ? `${process.env.NEXT_PUBLIC_PAGE_HOST}${process.env.NEXT_PUBLIC_BASE_PATH}${src}`
-      : `${process.env.NEXT_PUBLIC_BASE_PATH}${src}`
+    process.env.NODE_ENV === 'production' ? src : `${process.env.NEXT_PUBLIC_BASE_PATH}${src}`
   return <NextImage src={imageSrc} {...rest} />
 }
 
